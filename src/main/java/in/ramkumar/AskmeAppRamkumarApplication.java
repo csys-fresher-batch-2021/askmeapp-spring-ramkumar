0d2d0631d81e70b8d749ee8f3954239c3fe69af2
Ramkumar G <ramkumar.g.0420@gmail.com>
2021-06-18 12:57:46 +0530

    initial commit

---
+++ src/main/java/in/ramkumar/AskmeAppRamkumarApplication.java
@@ -0,0 +1,13 @@
+package in.ramkumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskmeAppRamkumarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskmeAppRamkumarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/ramkumar/AskmeAppRamkumarApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ramkumar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskmeAppRamkumarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
